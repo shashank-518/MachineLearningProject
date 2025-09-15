ad48a3b040c48aab4988e700ea18d785bf3d38f4
Shashank S <shashank5418shashu@gmail.com>
2025-09-16 00:32:57 +0530

    TAsk

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)-> List[str]:
+    requirements=[]
+    with open(file_path) as path:
+        requirements= path.readlines()
+        requirements = [ req.replace("/n" , "")  for req in requirements]
+
+
+setup(
+    name= "ML_PROJECT",
+    version='0.0.1',
+    author='Shashank S',
+    author_email='shashank5418shashu@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
